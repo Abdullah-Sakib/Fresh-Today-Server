0a3d07bc6c8c753b0daa463ecf7035212bab0d30
Mehedi Hasan <you@example.com>
2022-12-27 16:16:26 +0600

    create and update completed

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Fresh today server is running.')
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// mongodb database 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bydhqyw.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const productsCollection = client.db('freshDb').collection('productsCollections');
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result)
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error));
+
+
+app.get('/', async(req, res)=>{
+    res.send('the fresh today server is running');
 })
 
-app.listen(port, () => {
-  console.log(`Fresh today server is running on port ${port}.`)
+app.listen(port, ()=>{
+    console.log(`the fresh today server is running on ${port}`);
 })
