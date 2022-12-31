407135f15ad781adcc684417a85bfdd874e42e4b
Akil Chowdhury <akiluddinchy@gmail.com>
2022-12-31 14:26:28 +0600

    modify user data send after login to now not include password

--- src/routeHandler/usersHandler.js
+++ src/routeHandler/usersHandler.js
@@ -43,6 +43,7 @@ router.get('/', (req, res) => {
     router.post('/login', async (req, res) => {
         const email = req.body.userEmail;
         const user = await User.find({email: email});
+        const userInfo = {email: user[0].email, username: user[0].username, role: user[0].role};
         if(user == null){
             res.status(401).send('Authentication failed');
         }
@@ -50,7 +51,7 @@ router.get('/', (req, res) => {
             const isValidPassword = await bcrypt.compare(req.body.userPassword, user[0].password);
             if(isValidPassword){
                 const token = jwt.sign({email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
-                res.send({user: user[0], accessToken: token});
+                res.send({user: userInfo, accessToken: token});
             }
             else{
                 res.status(401).send('Authentication failed');
